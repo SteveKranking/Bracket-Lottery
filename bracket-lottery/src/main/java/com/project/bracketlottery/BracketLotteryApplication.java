b991a46251d4084a17bfcf4f73235506b0a7c8ab
SteveKranking <stephenkranking@yahoo.com>
2024-02-17 16:41:21 -0500

    Add initial spring framework

---
+++ bracket-lottery/src/main/java/com/project/bracketlottery/BracketLotteryApplication.java
@@ -0,0 +1,13 @@
+package com.project.bracketlottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BracketLotteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BracketLotteryApplication.class, args);
+	}
+
+}
