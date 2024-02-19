1aa6e0d245de21ce1dbfabb3236e03b83e205a68
SteveKranking <stephenkranking@yahoo.com>
2024-02-19 00:17:13 -0500

    Application seems to be starting on bootrun. DB Connection added

---
+++ bracket-lottery-backend/src/main/java/com/example/bracketlotterybackend/BracketLotteryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bracketlotterybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BracketLotteryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BracketLotteryBackendApplication.class, args);
+	}
+
+}
---
+++ bracket-lottery-backend/src/test/java/com/example/bracketlotterybackend/BracketLotteryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bracketlotterybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BracketLotteryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
