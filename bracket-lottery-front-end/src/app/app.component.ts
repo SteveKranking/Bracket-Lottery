f8e0decbb7dddde882f5b8b324c90b72c34a0cdb
SteveKranking <stephenkranking@yahoo.com>
2024-02-25 01:15:34 -0500

    Began Login Page ui

--- bracket-lottery-front-end/src/app/app.component.ts
+++ bracket-lottery-front-end/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './components/navbar/navbar.component';
+import {LoginScreenComponent} from './components/login-screen/login-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, LoginScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
