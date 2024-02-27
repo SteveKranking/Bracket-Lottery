823778d95fd369df8dda5350795d60c46c9a4e63
SteveKranking <stephenkranking@yahoo.com>
2024-02-27 18:32:10 -0500

    Begin home page, add more routing

--- bracket-lottery-front-end/src/app/app.routes.ts
+++ bracket-lottery-front-end/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {LoginScreenComponent} from './components/login-screen/login-screen.component';
 import {CreateAccountComponent} from './components/create-account/create-account.component';
+import {HomePageComponent} from './components/home-page/home-page.component';
 
 export const routes: Routes = [
     { path: '', component: LoginScreenComponent },
     { path: 'login-component', component: LoginScreenComponent },
     { path: 'create-account-component', component: CreateAccountComponent },
+    { path: 'home-page-component', component: HomePageComponent },
 ];
