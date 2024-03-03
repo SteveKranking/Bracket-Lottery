495754c5462e28f1ab9f8ccf066708f69e79b970
unknown <zjtjensen@yahoo.com>
2024-03-03 00:17:01 -0500

    fix: fixed backend to allow on port 8080

--- backend/src/main/java/backet/lottery/backend/BracketLotteryBackendApplication.java
+++ backend/src/main/java/backet/lottery/backend/BracketLotteryBackendApplication.java
@@ -2,8 +2,9 @@ package backet.lottery.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BracketLotteryBackendApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +12,4 @@ public class BracketLotteryBackendApplication {
 	}
 
 }
+	
--- backend/src/main/java/backet/lottery/backend/controllers/AccountController.java
+++ backend/src/main/java/backet/lottery/backend/controllers/AccountController.java
@@ -1,30 +1,15 @@
 package backet.lottery.backend.controllers;
 
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import backet.lottery.backend.repos.AccountRepository;
-import backet.lottery.backend.services.AccountService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/account")
 public class AccountController {
-    
-	@Autowired
-	private AccountService accountService;
-	
-	@Autowired
-    private AccountRepository accountRepository;
     
     @PostMapping("/createAccount")
 	public void createAccount(@RequestBody String message) {
